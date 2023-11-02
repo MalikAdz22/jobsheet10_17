bf1135444989739a41d0f772b57aea2b70edaf68
MalikAdz22 <adzanomalik@gmail.com>
2023-11-01 17:38:16 -0800

    Bioskop3

--- Bioskop17.java
+++ Bioskop17.java
@@ -28,10 +28,16 @@ public class Bioskop17 {
             System.out.println("Panjang baris: " + barisPenonton.length);
            }
 
-           System.out.println("Penonton pada baris ke-3: ");
-           for (int i = 0; i < penonton[2].length; i++){
-            System.out.println(penonton[2][i]);
-           }
+        //    System.out.println("Penonton pada baris ke-3: ");
+        //    for (int i = 0; i < penonton[2].length; i++){
+        //     System.out.println(penonton[2][i]);
+        //    }
+
+            System.out.println("Penonton pada baris ke-3 ");
+            for (String i : penonton[2]){
+                System.out.println(i);
+            }
+
            System.out.println(penonton[0].length);
            System.out.println(penonton[1].length);
            System.out.println(penonton[2].length);
